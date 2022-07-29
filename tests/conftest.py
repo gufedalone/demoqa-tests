40aadfa4e12a543b18db7b75a5cdca1aeab790cc
vlad <khalvlalove@gmail.com>
2022-07-29 15:08:36 +0400

    add browser name

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ import os
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
+    browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
     browser.config.hold_browser_open = (
         os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
     )
