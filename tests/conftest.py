8e5bf0a00efc5a3387a90c619ebb899f9f50de15
vlad <khalvlalove@gmail.com>
2022-07-29 16:09:45 +0400

    clean up the mess

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,15 +5,18 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-DEFAULT_BROWSER_VERSION = '100.0'
 
 with allure.step('Browser Management'):
     @pytest.fixture(scope='function', autouse=True)
-    def browser_management(request):
-        remote_driver = request.config.getoption('--remote_driver')
-        remote_driver_version = remote_driver if remote_driver != "" else DEFAULT_BROWSER_VERSION
-        options = Options()
+    def browser_management():
+        browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
+        browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
+        browser.config.hold_browser_open = (
+                os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
+        )
+        browser.config.timeout = float(os.getenv('selene.timeout', '3'))
 
+        options = Options()
         selenoid_capabilities = {
             "browserName": "chrome",
             "browserVersion": "100.0",
@@ -22,16 +25,15 @@ with allure.step('Browser Management'):
                 "enableVideo": False
             }
         }
-
         options.capabilities.update(selenoid_capabilities)
-        browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
-        browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
-        browser.config.hold_browser_open = (
-                os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
-        )
-        browser.config.timeout = float(os.getenv('selene.timeout', '3'))
         driver = webdriver.Remote(
-            command_executor=f"https://user1:1234@{remote_driver_version}/wd/hub",
-            desired_capabilities=selenoid_capabilities
+            command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+            options=options
         )
         browser.config.driver = driver
+
+    def pytest_addoption(parser):
+        parser.addoption(
+            '--browser_version',
+            default='100.0'
+            )
