96039aeb65cf60cc2c78f4c7cdd739996a2ec07c
gufedalone <107944718+gufedalone@users.noreply.github.com>
2022-07-29 21:05:03 +0400

    Jenkins (#2)
    add jenkins version

--- model/app.py
+++ model/app.py
@@ -1,14 +1,23 @@
+import time
 from selene import have, command
-from selene.support.shared import browser
+from tests.conftest import browser
 from model.pages.student_registration_page import StudentRegistrationForm, RegisteredUser
+from utils import attach
 
 form = StudentRegistrationForm()
 results = RegisteredUser()
 
 
-def open_form_page():
-    browser.open('/automation-practice-form')
-
+def open_form_page(url: str):
+    browser.open(url)
+    time.sleep(1)
     browser.all('[id^=google_ads][id$=container__],[id$=Advertisement]') \
         .with_(timeout=10).should(have.size_greater_than_or_equal(3)) \
         .perform(command.js.remove)
+
+
+def add_attachments():
+    attach.add_screenshot()
+    attach.add_logs()
+    attach.add_html()
+    attach.add_video()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,42 @@
+import os
+from dotenv import load_dotenv
 from selene.support.shared import browser
 import pytest
-import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv("LOGIN")
+    password = os.getenv("PASSWORD")
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
     browser.config.hold_browser_open = (
-        os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
+            os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
     )
     browser.config.timeout = float(os.getenv('selene.timeout', '3'))
+
+    yield browser
