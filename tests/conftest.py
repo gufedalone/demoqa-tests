bae45c670909fce49d275eeb1621f78dbee020bb
vlad <khalvlalove@gmail.com>
2022-07-29 16:17:35 +0400

    clean up the mess

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,18 +6,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-with allure.step('Browser Management'):
-    @pytest.fixture(scope='function', autouse=True)
-    def browser_management():
-        browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
-        browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
-        browser.config.hold_browser_open = (
-                os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
-        )
-        browser.config.timeout = float(os.getenv('selene.timeout', '3'))
-
-        options = Options()
-        selenoid_capabilities = {
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+    browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
+    browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
+    browser.config.timeout = float(os.getenv('selene.timeout', '3'))
+    options = Options()
+    selenoid_capabilities = {
             "browserName": "chrome",
             "browserVersion": "100.0",
             "selenoid:options": {
@@ -25,15 +20,11 @@ with allure.step('Browser Management'):
                 "enableVideo": False
             }
         }
-        options.capabilities.update(selenoid_capabilities)
-        driver = webdriver.Remote(
-            command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-            options=options
-        )
-        browser.config.driver = driver
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.driver = driver
 
-    def pytest_addoption(parser):
-        parser.addoption(
-            '--browser_version',
-            default='100.0'
-            )
+    yield browser
