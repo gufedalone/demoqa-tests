ae383ee60bc94d35bd7f21cebd9b15c446dbbee1
vlad <khalvlalove@gmail.com>
2022-07-29 15:56:06 +0400

    clean up the mess

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,13 +5,13 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-DEFAULT_REMOTE_DRIVER = 'selenoid.autotests.cloud'
+DEFAULT_BROWSER_VERSION = '100.0'
 
 with allure.step('Browser Management'):
     @pytest.fixture(scope='function', autouse=True)
     def browser_management(request):
         remote_driver = request.config.getoption('--remote_driver')
-        remote_driver_option = remote_driver if remote_driver != "" else DEFAULT_REMOTE_DRIVER
+        remote_driver_version = remote_driver if remote_driver != "" else DEFAULT_BROWSER_VERSION
         options = Options()
 
         selenoid_capabilities = {
@@ -24,21 +24,14 @@ with allure.step('Browser Management'):
         }
 
         options.capabilities.update(selenoid_capabilities)
-
         browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
         browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
         browser.config.hold_browser_open = (
                 os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
         )
         browser.config.timeout = float(os.getenv('selene.timeout', '3'))
-        browser.config.driver = webdriver.Remote(
-            command_executor=f"https://user1:1234@{remote_driver_option}/wd/hub",
+        driver = webdriver.Remote(
+            command_executor=f"https://user1:1234@{remote_driver_version}/wd/hub",
             desired_capabilities=selenoid_capabilities
         )
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--remote_driver',
-        default='selenoid.autotests.cloud'
-    )
+        browser.config.driver = driver
