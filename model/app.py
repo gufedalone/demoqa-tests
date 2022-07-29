7f12c6b50f9e3883373f698a58004ebc9ea7b43f
gufedalone <107944718+gufedalone@users.noreply.github.com>
2022-07-29 21:07:04 +0400

    Revert "Jenkins (#2)"
    This reverts commit 96039aeb65cf60cc2c78f4c7cdd739996a2ec07c.

--- model/app.py
+++ model/app.py
@@ -1,23 +1,14 @@
-import time
 from selene import have, command
-from tests.conftest import browser
+from selene.support.shared import browser
 from model.pages.student_registration_page import StudentRegistrationForm, RegisteredUser
-from utils import attach
 
 form = StudentRegistrationForm()
 results = RegisteredUser()
 
 
-def open_form_page(url: str):
-    browser.open(url)
-    time.sleep(1)
+def open_form_page():
+    browser.open('/automation-practice-form')
+
     browser.all('[id^=google_ads][id$=container__],[id$=Advertisement]') \
         .with_(timeout=10).should(have.size_greater_than_or_equal(3)) \
         .perform(command.js.remove)
-
-
-def add_attachments():
-    attach.add_screenshot()
-    attach.add_logs()
-    attach.add_html()
-    attach.add_video()
